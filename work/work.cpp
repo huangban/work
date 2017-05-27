5b0c38acfa755e333f636de9d592e5b4c21b4c3b
huangban <786992676.com>
2017-05-27 19:29:19 +0800

    t it
    t#

--- work/work.cpp
+++ work/work.cpp
@@ -11,51 +11,6 @@ using namespace std;
 
 int main()
 {
-	string str;
-	getline(cin, str);
-	if(str.empty())
-	{
-		cout << 0 << endl;	
-		return 0;
-	}
-	int len = str.length();
-	if(len > 128)
-		return -1;
-	int index1;
-	bool flag = false;
-	for(int i=len-1;i>=0;i--)
-	{
-		if(str[i] != ' ')
-		{
-			flag = true;
-			index1 = i;
-			break;
-		}
-	}
-	if(flag == false)
-	{
-		cout << 0 << endl;	
-		return 0;
-	}
-	int index2 = 0;
-	bool flag2 = false;
-	for(int i=index1;i>=0;i--)
-	{
-		if(str[i] == ' ')
-		{
-			flag2 = true;
-			index2 = i;
-			break;
-		}
-	}
-	if(flag2 == false)
-	{
-		cout << index1+1 << endl;
-		return 0;
-	}
-	int wl = index1 - index2;
-
-	cout << wl << endl;
 
     return 0;
 }
